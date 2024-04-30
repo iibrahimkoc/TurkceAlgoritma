a82718e67b22df0581e001b005627798f4d603e5
İbrahim Koç <112804288+iibrahimkoc@users.noreply.github.com>
2024-04-30 11:03:27 +0300

    update

--- Fibo/ibrahimkoc_2211502004.java
+++ Fibo/ibrahimkoc_2211502004.java
@@ -6,18 +6,22 @@ public class ibrahimkoc_2211502004 {
         System.out.print("Eleman değerini gir: ");
         int value = input.nextInt();
         long time1 = System.nanoTime();
-        fibonacciDizi(value - 2);
+        fibonacciDizi(value);
         long time2 = System.nanoTime();
         System.out.println("Time: " + (time2 - time1) / 1000);
     }
 
     public static void fibonacciDizi(int value) {
-        int num1 = 0;
-        int num2 = 1;
-        int temp;
+        long num1 = 0;
+        long num2 = 1;
+        long temp;
 
-        System.out.println(num1);
-        System.out.println(num2);
+        if (value == 1) {
+            System.out.println(num1);
+            return;
+        } else {
+            System.out.println(num1);
+        }
         for (int i = 0; i < value; i++) {
             temp = num1 + num2;
             num1 = num2;
